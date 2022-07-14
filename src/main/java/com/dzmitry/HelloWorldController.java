f016d2316ec17003afbf86448bcffc241458f1cf
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-07-14 18:21:46 +0200

    binding request params

--- src/main/java/com/dzmitry/HelloWorldController.java
+++ src/main/java/com/dzmitry/HelloWorldController.java
@@ -2,7 +2,9 @@ package com.dzmitry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,6 +29,19 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo! " + theName;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey My Friend! " + theName;
 
         model.addAttribute("message", result);
 
