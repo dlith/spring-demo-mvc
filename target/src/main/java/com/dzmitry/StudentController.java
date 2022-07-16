03a941860fbb6667531523e0b6ecb721474ffbed
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-07-16 12:59:01 +0200

    radiobutton spring form

---
+++ target/src/main/java/com/dzmitry/StudentController.java
@@ -0,0 +1,32 @@
+package com.dzmitry;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @Value("#{countryOptions}")
+    private Map<String, String> countryOptions;
+
+    @RequestMapping("/showForm")
+    public String showForm(Model model){
+        model.addAttribute("student", new Student());
+        model.addAttribute("theCountryOptions", countryOptions);
+        return "student-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@ModelAttribute("student") Student student){
+
+        System.out.println("Our student: " + student);
+
+        return "student-confirmation";
+    }
+}
---
+++ target/src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Spring MVC demo - Home page</h2>
+    <hr>
+    <a href="hello/showForm">Hello World form</a>
+    <br><br>
+    <a href="student/showForm">Student form</a>
+    <hr>
+</body>
+</html>
